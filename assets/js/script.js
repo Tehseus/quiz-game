5651f8ceea140c6afa5b66c041ed9ad43b56d16e
badtiming <graysonliv@gmail.com>
2022-08-09 09:35:51 -0400

    start button created, timer function created, start game function created

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+var startButton = document.querySelector(".start-button");
+
+
+function runTimer(){
+    console.log("timer started");{
+        var timeLeft = 60;
+      
+        // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+        var timerEl = document.querySelector(".timer-count")
+        var timeInterval = setInterval(function () {
+          // As long as the `timeLeft` is greater than 1
+          if (timeLeft > 1) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = timeLeft
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+          } else if (timeLeft === 1) {
+            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = timeLeft
+            timeLeft--;
+          } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = '0';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Call the `displayMessage()` function
+            
+          }
+        }, 1000);
+      }
+}
+
+
+startButton.addEventListener("click", startGame);
