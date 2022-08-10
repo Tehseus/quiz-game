edfd13aa398f1c000f3da5af25195b83efd4057b
badtiming <graysonliv@gmail.com>
2022-08-10 01:29:17 -0400

    added content to question/answer array, able to pull questions to display, cannot currently pull answers into answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
+
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
@@ -9,31 +7,152 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+
 var startButton = document.querySelector(".start-button");
+var questionContainerElement = document.getElementById('questionContainer')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answerContainer')
+var questionContainerElement = document.getElementById("questionContainer")
+var answer1 = ""
+var answer2 = ""
+var answer3 = ""
+var answer4 = ""
+var userSelection = ""
+var startingScore = 0
+var score = 0
 
-//arrays for questions/answers
-questionOne = ["","","",""];
-questionTwo = ["","","",""];
-questionThree = ["","","",""];
-questionFour = ["","","",""];
-questionFive = ["","","",""];
-questionSix = ["","","",""];
-questionSeven = ["","","",""];
-questionEight = ["","","",""];
-questionNine = ["","","",""];
-questionTen = ["","","",""];
+let randomQuestions = undefined;
+let currentQuestionIndex =undefined;
+//function that starts the game and calls the timer function
+//also unhides the questions
+function startGame() {
+    runTimer();
+    questionContainerElement.classList.remove("hide")
+    randomQuestions = questions.sort(()=> Math.random()- .5)
+    currentQuestionIndex = 0
+    setNextQuestion();
+    selectedAnswer();
+    
+}
 
-questionOneAnswer = "";
-questionTwoAnswer = "";
-questionThreeAnswer = "";
-questionFourAnswer = "";
-questionFiveAnswer = "";
-questionSixAnswer = "";
-questionSevenAnswer = "";
-questionEightAnswer = "";
-questionNineAnswer = "";
-questionTenAnswer = "";
 
+//function to set the next question
+function setNextQuestion() {
+    showQuestion(randomQuestions[currentQuestionIndex])
+}
+//function to pull questions
+function showQuestion(questions) {
+    questionElement.innerText = questions.question
+}
+function selectedAnswer() {
+    answerButtonsElement[currentQuestionIndex].innerText = document.getElementById("answerContainer").child[currentQuestionIndex];
+    console.log(answerButtonsElement[currentQuestionIndex].innerText);
+
+    
+}
+
+//question arrays holding my questions/answers
+//swapped to using const instead of 10 different arrays for questions/answers
+const questions = [
+    {
+        question: 'How many teams are in the NHL?',
+        answers: [
+          { text: '31', correct: false },
+          { text: '28', correct: false },
+          { text: '32', correct: true },
+          { text: '30', correct: false }
+        ]
+      },
+    {
+        question: 'Which NHL player is called the Goat?',
+        answers: [
+          { text: 'Mario Lemieux', correct: false },
+          { text: 'Conor McDavid', correct: false },
+          { text: 'Wayne Gretzky', correct: true },
+          { text: 'Pavel Datsyuk', correct: false }
+        ]
+      },
+    {
+        question: 'Who owns the hardest shot record in NHL history?',
+        answers: [
+          { text: 'Shea Weber', correct: false },
+          { text: 'Zdeno Chara', correct: true },
+          { text: 'Alexander Ovechkin', correct: false },
+          { text: 'Patrik Laine', correct: false }
+        ]
+      },
+    {
+        question: 'Who wins the Vezina Trophy',
+        answers: [
+          { text: "The league's best Goalie.", correct: true },
+          { text: `The League's best Winger`, correct: false },
+          { text: `The League's best Center`, correct: false },
+          { text: `The League's best Defenseman`, correct: false}
+        ]
+    },          
+    {
+        question: 'Which NHL season was cancelled?',
+        answers: [
+          { text: '2004-2005', correct: true },
+          { text: '2005-2006', correct: false },
+          { text: '2003-2004', correct: false },
+          { text: '2006-2007', correct: false }
+        ]
+      },
+    {
+        question: 'How many goals makes a hat trick?',
+        answers: [
+          { text: '2', correct: false },
+          { text: '3', correct: true },
+          { text: '4', correct: false },
+          { text: '5', correct: false }
+        ]
+      },
+    {
+        question: 'How long is a minor penalty served?',
+        answers: [
+          { text: '1.5 minutes', correct: false },
+          { text: '2 minutes', correct: false },
+          { text: '3 minutes', correct: false },
+          { text: '5 minutes', correct: false }
+        ]
+      },
+    {
+        question: 'Out of the 31 NHL teams how many of them are based out of Canada?',
+        answers: [
+          { text: '9', correct: false },
+          { text: '6', correct: false },
+          { text: '8', correct: false },
+          { text: '7', correct: true }
+        ]
+      },
+    {
+        question: 'Which former player is popularly called "Mr.Hockey"?',
+        answers: [
+          { text: 'Wayne Gretzky', correct: false },
+          { text: 'Gordie Howe', correct: true },
+          { text: 'Alex Ovechkin', correct: false },
+          { text: 'Bobby Orr', correct: false }
+        ]
+      },
+    {
+        question: 'Which trophy is given to the most valuable player?',
+        answers: [
+          { text: 'The Selke Trophy', correct: false },
+          { text: 'The Vezina Trophy', correct: false },
+          { text: 'The Hart Trophy', correct: false },
+          { text: 'The Masterton Trophy', correct: false }
+        ]
+      },
+      
+
+]
+
+
+
+
+
+// THEN a timer starts and I am presented with a question
 function runTimer(){
     console.log("timer started");{
         var timeLeft = 60;
@@ -62,70 +181,8 @@ function runTimer(){
         }, 1000);
       }
 }
-var questionContainerElement = document.getElementById("questionContainer")
-function startGame() {
-    runTimer();
-    questionContainerElement.classList.remove("hide")
-//display questions, loop through them as questions are answered, tally/subtract points/time as needed
-    
-}
-startButton.addEventListener("click", startGame);
-var userSelection = ""
-var startingScore = 0
-var score = 0
 
-for (i = 0; i < array.length; i++) {
-    const element = array[i];
-    if (questionOneAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionTwoAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionThreeAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionFourAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionFiveAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionSixAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionSevenAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionEightAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionNineAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    if (questionTenAnswer===userSelection) {
-        score = startingScore + 10
-    } else {
-        score = startingScore - 10
-    }
-    
-    
-}
+
+// WHEN I click the start button
+startButton.addEventListener("click", startGame);
+
